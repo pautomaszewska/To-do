5e820c1009cce24e99fc1a9455af11a7058565a9
pautomaszewska <ptomaszewska@gmail.com>
2019-10-12 14:16:37 +0200

    Added basic functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('test');
+const add = $('#add');
+const remove = $('#remove');
+const clear = $('#clear');
+let list = $('#list');
+let number = 0;
+
+add.on('click', function () {
+    number ++;
+    list.append($('<li>', {text: "Task number " + number}))
+});
+
+remove.on('click', function () {
+    let last = list.children().last();
+    last.remove();
+    number --
+});
+
+clear.on('click', function () {
+    list.empty();
+    number = 0
+});
