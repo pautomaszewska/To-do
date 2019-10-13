c86af6b705e0ecf5b731829b4bc25ec1ca452e42
pautomaszewska <ptomaszewska@gmail.com>
2019-10-13 15:24:09 +0200

    Disabled adding identical tasks

--- js/app.js
+++ js/app.js
@@ -4,15 +4,26 @@ const clear = $('#clear');
 let list = $('#list');
 let number = 0;
 let newtask = $('#newtask');
+let tasklist = [];
 
-console.log(newtask)
+function contains(arr, obj) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 
 add.on('click', function () {
-    if (newtask.val() !== '') {
+    if (newtask.val() !== '' && contains(tasklist, newtask.val())===false) {
         number ++;
-        list.append($('<div>', {text: "Task number " + number + ': ' + newtask.val(), class: 'task'}))
+        list.append($('<div>', {text: "Task number " + number + ': ' + newtask.val(), class: 'task'}));
         remove.attr('hidden', false);
-        clear.attr('hidden', false)
+        clear.attr('hidden', false);
+        tasklist.push(newtask.val());
+    } else if (contains(tasklist, newtask.val())===true){
+        alert('This task already exists!')
     } else {
         alert('Task name cannot be empty!')
     }
@@ -23,6 +34,7 @@ remove.on('click', function () {
     let last = list.children().last();
     last.remove();
     number --;
+    tasklist.pop(last);
     if (number==0) {
         $(this).attr('hidden', true);
         clear.attr('hidden', true)
@@ -32,6 +44,7 @@ remove.on('click', function () {
 clear.on('click', function () {
     list.empty();
     number = 0;
+    tasklist=[];
     $(this).attr('hidden', true);
     remove.attr('hidden', true)
 });
